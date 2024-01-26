4508cc651dff4135fbdb00973631ebda37bce915
Danya <dan.shell@bk.ru>
2024-01-26 20:53:29 +0300

    add locators and login page

---
+++ locators.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+class RegistrationPageLocators():
+    BUTTON=(By.CSS_SELECTOR,"")
+    LOGIN=(By.CSS_SELECTOR,"")
+    PASSWORD=(By.CSS_SELECTOR,"")
+    LOGO=(By.CSS_SELECTOR,"")
---
+++ login_page.py
@@ -0,0 +1,2 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
