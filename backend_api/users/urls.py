f3fe8626f780b36e66b38ba5c9996a5083cf3ae5
Alexsandr <artamoshkinas@gmail.com>
2024-01-27 22:24:05 +0700

    Исправлено: Url для регистрации с registration на registr

--- backend_api/users/urls.py
+++ backend_api/users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import UserCreatView
 
 urlpatterns = [
-    path('registration', UserCreatView.as_view()),
+    path('register', UserCreatView.as_view()),
 ]
