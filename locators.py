3ae81106b2a443c994b1f6f160f2bf383766307b
Danya <dan.shell@bk.ru>
2024-01-29 17:59:29 +0300

    1 variant without test cases

--- locators.py
+++ locators.py
@@ -5,3 +5,11 @@ class RegistrationPageLocators():
     LOGIN=(By.CSS_SELECTOR,"")
     PASSWORD=(By.CSS_SELECTOR,"")
     LOGO=(By.CSS_SELECTOR,"")
+    RETURN_TO_AUTH=(By.CSS_SELECTOR,"")
+
+class LoginPageLocators():
+    BUTTON=(By.CSS_SELECTOR,"")
+    LOGIN=(By.CSS_SELECTOR,"")
+    PASSWORD=(By.CSS_SELECTOR,"")
+    LOGO=(By.CSS_SELECTOR,"")
+    RETURN_TO_LOGIN=(By.CSS_SELECTOR,"")
--- login_page.py
+++ login_page.py
@@ -1,2 +1,19 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import LoginPageLocators
+from .variables import LoginPageVariables
+
+class LoginPage(BasePage):
+
+        def fill_login(self):
+            login=self.enter_text(*LoginPageLocators.LOGIN,f"{LoginPageVariables.LOGIN}")
+
+        def fill_password(self):
+
+            password=self.enter_text(*LoginPageLocators.PASSWORD,f"{LoginPageVariables.PASSWORD}")
+
+        def click_registration_button(self):
+
+            button=self.click_element(*LoginPageLocators.BUTTON)
+
+
