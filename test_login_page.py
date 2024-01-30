97b55e94e61e2f567ce478a8259a16ddf0cdaeda
Danya <dan.shell@bk.ru>
2024-01-30 10:48:34 +0300

    add conftest.py and test_login_page.py

---
+++ test_login_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from .pages.login_page import LoginPage
+import pytest
+
+
--- variables.py
+++
@@ -1,3 +0,0 @@
-class LoginPageVariables():
-    LOGIN=""
-    PASSWORD=""
