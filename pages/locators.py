93db1d9336af7a576fbb2eb5013f8d1f896894f0
Danya <dan.shell@bk.ru>
2024-01-30 10:15:21 +0300

    add folder pages

---
+++ pages/locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+class RegistrationPageLocators():
+    BUTTON=(By.CSS_SELECTOR,"")
+    LOGIN=(By.CSS_SELECTOR,"")
+    PASSWORD=(By.CSS_SELECTOR,"")
+    LOGO=(By.CSS_SELECTOR,"")
+    RETURN_TO_AUTH=(By.CSS_SELECTOR,"")
+
+class LoginPageLocators():
+    BUTTON=(By.CSS_SELECTOR,"")
+    LOGIN=(By.CSS_SELECTOR,"")
+    PASSWORD=(By.CSS_SELECTOR,"")
+    LOGO=(By.CSS_SELECTOR,"")
+    RETURN_TO_LOGIN=(By.CSS_SELECTOR,"")
---
+++ pages/variables.py
@@ -0,0 +1,3 @@
+class LoginPageVariables():
+    LOGIN=""
+    PASSWORD=""
