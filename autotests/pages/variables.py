2efb3029ccb6a646e6ed81be0011f5b9a6e493b0
Danya <dan.shell@bk.ru>
2024-01-31 17:19:30 +0300

    create test_auth_page.py

--- autotests/pages/variables.py
+++ autotests/pages/variables.py
@@ -1,9 +1,14 @@
 incorrect_data={
-      SHORT_LOGIN :"sd" ,
-      SHORT_PASSWORD:"123"
+      "SHORT_LOGIN" :"sd" ,
+      "SHORT_PASSWORD":"123"
+}
+correct_data={
+        "LOGIN":"jbblkb",
+        "PASSWORD":"m l k",
 
 
     }
+
 
 class RegistrationPageVariables():
     LOGIN=""
---
+++ autotests/test_auth_page.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from .pages.authorization_page import AuthorizationPage
+import pytest
+from .pages.variables import correct_data
+
+link='example_link'
+
+
