5465e613914507735946388a5b6f3216a8424118
Danya <dan.shell@bk.ru>
2024-02-03 18:42:47 +0300

    create test folder and move test scripts there

--- autotests/tests/test_auth_page.py
+++ autotests/tests/test_auth_page.py
@@ -3,9 +3,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from .pages.authorization_page import AuthorizationPage
+from ..pages.authorization_page import AuthorizationPage
 import pytest
-from .variables.variables import correct_data, incorrect_data
+from ..variables.variables import correct_data, incorrect_data
 
 link='example_link'
 
--- autotests/tests/test_registration_page.py
+++ autotests/tests/test_registration_page.py
@@ -3,9 +3,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from .pages.registration_page import RegistrationPage
+from ..pages.registration_page import RegistrationPage
 import pytest
-from .variables.variables import correct_data
+from ..variables.variables import correct_data
 
 link='example_link'
 
