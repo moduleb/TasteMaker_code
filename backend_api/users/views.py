182a2d919fd676ae88191f90b44407cb766a93be
Alexsandr <artamoshkinas@gmail.com>
2024-02-08 23:47:04 +0700

    Исправлено: При регистрации пользователя в ответе от сервера приходит статус

--- backend_api/users/views.py
+++ backend_api/users/views.py
@@ -1,8 +1,16 @@
-from rest_framework import generics
-
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .serializers import UserSerializer
 
 
 class UserCreateView(generics.CreateAPIView):
     """Оправляет POST запрос для регистрации пользователя в БД"""
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        """Метод возвращает статус POST запроса"""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(status=status.HTTP_201_CREATED)
