24b6e7de42fd676bbbffbec976e548e11285b70b
Aroptich <artamoshkinas@gmail.com>
2024-02-25 11:22:02 +0700

    Исправлено: заменен метод create в отображении user на метод post

--- backend_api/users/views.py
+++ backend_api/users/views.py
@@ -11,13 +11,14 @@ class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny] #Создать пользователя могут не авторизированные пользователи
 
-    def create(self, request, *args, **kwargs):
-        """Метод возвращает статус POST запроса"""
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(status=status.HTTP_201_CREATED)
+
+
 
 class UserRUDView(generics.RetrieveUpdateDestroyAPIView):
     """Представление модели Пользователя"""
