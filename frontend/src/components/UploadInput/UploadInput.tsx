302e58c29fd71148e0841d76d50652f58be02b09
Anton <wnfgnqpw@gmail.com>
2024-02-26 16:16:46 +0300

    добавил страницу добавления рецепта. сделал вывод рецептов на главной

--- frontend/src/components/UploadInput/UploadInput.tsx
+++ frontend/src/components/UploadInput/UploadInput.tsx
@@ -1,5 +1,5 @@
 import s from "./UploadInput.module.css"
-import { ChangeEventHandler } from "react"
+import { ChangeEventHandler, MutableRefObject, useRef } from "react"
 
 interface Props {
   uploadFile: (file: File) => void
@@ -7,9 +7,19 @@ interface Props {
 
 export const UploadInput = ({ uploadFile }: Props) => {
   // const [file, setFile] = useState<null | File>(null)
+  const preview: MutableRefObject<null | HTMLImageElement> = useRef(null)
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     if (e.target.files) {
+      const reader = new FileReader()
+      console.log(reader)
+      reader.onload = () => {
+        console.log(1)
+        if (preview.current) {
+          preview.current.src = reader.result
+        }
+      }
+      reader.readAsDataURL(e.target.files[0])
       uploadFile(e.target.files[0])
     }
   }
@@ -17,6 +27,7 @@ export const UploadInput = ({ uploadFile }: Props) => {
   return (
     <label>
       <input type="file" onChange={onChange} />
+      <img src="" ref={preview} alt="" />
     </label>
   )
 }
